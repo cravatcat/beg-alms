fbd1b698e0c3cb8d6be379ab0bb44dbf4644f483
kazuyaWang <4740721@qq.com>
2025-08-24 13:11:55 +0800

    add: gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
+  plugins: [react()],
+  base: '/beg-alms/', // 这里要与你的仓库名一致
+  build: {
+    outDir: 'dist'
+  }
 })
